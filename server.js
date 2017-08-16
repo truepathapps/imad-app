57373b1a5e9e25169dbb7588e4fdfce4c4e07b9b
truepathapps <30745022+truepathapps@users.noreply.github.com>
2017-08-16 20:17:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/arcticle-one', function (req, res) {
-  res.send('Artcle one is requested and will be server here');
+app.get('/article-one', function (req, res) {
+  res.send('Article one is requested and will be server here');
 });
-app.get('/arcticle-two', function (req, res) {
-  res.send('Artcle two is requested and will be server here');
+app.get('/article-two', function (req, res) {
+  res.send('Article two is requested and will be server here');
 });
-app.get('/arcticle-three', function (req, res) {
-  res.send('Artcle three is requested and will be server here');
+app.get('/article-three', function (req, res) {
+  res.send('Article three is requested and will be server here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
